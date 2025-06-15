b65c093e4a6749228f4ff68301efc7b601d7da90
Alexander Ubeda <91618251+ubeda-alex@users.noreply.github.com>
2025-06-14 18:18:06 -0600

    Se cambió la base de datos a la de Hoyner

--- App Bases de Datos/modulos/supabaseClient.js
+++ App Bases de Datos/modulos/supabaseClient.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = "https://bnuivotsgszcthkzqqvv.supabase.co";
+const supabaseUrl = "https://ksjrgtuelsubypybwoun.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJudWl2b3RzZ3N6Y3Roa3pxcXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NDU5ODIsImV4cCI6MjA2NTQyMTk4Mn0.adLFIf_xu-N-yor5nJE3hUGENalxjT-VwAjed7u3Mvc";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzanJndHVlbHN1YnlweWJ3b3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NjAzNjgsImV4cCI6MjA2NDAzNjM2OH0._bjhrHfSIl21_HBpncn1YnhpoCnFwhV6YjHdmHHFr-E";
 export const supabase = createClient(supabaseUrl, supabaseKey);
